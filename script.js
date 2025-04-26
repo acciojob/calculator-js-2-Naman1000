3bfbadcec25c171b50726899521a1a382a237aef
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-04-26 16:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let display = document.getElementById("display");
+let button =  document.querySelectorAll("button");
+
+buttons.forEach(i =>{
+	i.addEventListener("click", function(){
+		const id = this.id;
+
+		if(id==='C'){
+			display.value='';
+		}
+		else if(id === "back")
+	})
+})
